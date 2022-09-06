c1086c242f93bcea7547ed6e0b3a94d06fe6083d
Michael Amrhein <michael@adrhinum.de>
2022-09-06 13:31:52 +0200

    Added tests.

--- src/lib.rs
+++ src/lib.rs
@@ -437,6 +437,7 @@ mod tests {
     #[test]
     fn test_nan() {
         assert!(f256::NAN.is_nan());
+        assert!((-f256::NAN).is_nan());
         assert!(!f256::INFINITY.is_nan());
         assert!(!f256::NEG_INFINITY.is_nan());
     }
@@ -445,7 +446,15 @@ mod tests {
     fn test_inf() {
         assert!(f256::INFINITY.is_infinite());
         assert!(f256::NEG_INFINITY.is_infinite());
+        assert!(!f256::NAN.is_infinite());
+        assert!(!f256::NEG_ZERO.is_infinite());
+        assert!(!f256::ZERO.is_infinite());
+        assert!(!f256::ONE.is_infinite());
         assert!(!f256::INFINITY.is_finite());
         assert!(!f256::NEG_INFINITY.is_finite());
+        assert!(!f256::NAN.is_finite());
+        assert!(f256::NEG_ZERO.is_finite());
+        assert!(f256::ZERO.is_finite());
+        assert!(f256::ONE.is_finite());
     }
 }
