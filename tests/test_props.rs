94a6f1c9b0122c35b143d4245748060ee04b7ad4
Michael Amrhein <michael@adrhinum.de>
2022-09-29 19:53:19 +0200

    Added tests for fn f256::classify.

--- tests/test_props.rs
+++ tests/test_props.rs
@@ -7,14 +7,19 @@
 // $Source$
 // $Revision$
 
+extern crate core;
+
 #[cfg(test)]
 mod property_tests {
+    use core::num::FpCategory;
+
     use f256::f256;
 
     #[test]
     fn test_nan() {
         assert!(f256::NAN.is_nan());
         assert!((-f256::NAN).is_nan());
+        assert_eq!(f256::NAN.classify(), FpCategory::Nan);
         assert!(!f256::INFINITY.is_nan());
         assert!(!f256::NEG_INFINITY.is_nan());
         assert!(!f256::ZERO.is_nan());
@@ -26,7 +31,9 @@ mod property_tests {
     #[test]
     fn test_inf() {
         assert!(f256::INFINITY.is_infinite());
+        assert_eq!(f256::INFINITY.classify(), FpCategory::Infinite);
         assert!(f256::NEG_INFINITY.is_infinite());
+        assert_eq!(f256::NEG_INFINITY.classify(), FpCategory::Infinite);
         assert!(!f256::NAN.is_infinite());
         assert!(!f256::NEG_ZERO.is_infinite());
         assert!(!f256::ZERO.is_infinite());
@@ -44,7 +51,9 @@ mod property_tests {
     #[test]
     fn test_zero() {
         assert!(f256::ZERO.is_zero());
+        assert_eq!(f256::ZERO.classify(), FpCategory::Zero);
         assert!(f256::NEG_ZERO.is_zero());
+        assert_eq!(f256::NEG_ZERO.classify(), FpCategory::Zero);
         assert!(!f256::ONE.is_zero());
         assert!(!f256::NEG_ONE.is_zero());
         assert!(!f256::NAN.is_zero());
@@ -63,8 +72,11 @@ mod property_tests {
         assert!(!f256::ZERO.is_normal());
         assert!(!f256::NEG_ZERO.is_normal());
         assert!(f256::ONE.is_normal());
+        assert_eq!(f256::ONE.classify(), FpCategory::Normal);
         assert!(f256::NEG_ONE.is_normal());
+        assert_eq!(f256::NEG_ONE.classify(), FpCategory::Normal);
         assert!(f256::EPSILON.is_normal());
+        assert_eq!(f256::EPSILON.classify(), FpCategory::Normal);
         assert!(f256::MAX.is_normal());
         assert!(f256::MIN.is_normal());
         assert!(f256::MIN_POSITIVE.is_normal());
@@ -95,6 +107,7 @@ mod property_tests {
         assert!(!f256::from(f64::MAX).is_subnormal());
         assert!(!f256::from(1e-312_f64).is_subnormal());
         assert!(f256::MIN_GT_ZERO.is_subnormal());
+        assert_eq!(f256::MIN_GT_ZERO.classify(), FpCategory::Subnormal);
         assert!((-f256::MIN_GT_ZERO).is_subnormal());
     }
 
