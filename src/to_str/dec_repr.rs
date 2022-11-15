9053dd76de528fb8f6f633283db2e3cbd1d9ab15
Michael Amrhein <michael@adrhinum.de>
2022-11-15 20:26:30 +0100

    Optimized DecNumRepr::from_f256_shortest.

--- src/to_str/dec_repr.rs
+++ src/to_str/dec_repr.rs
@@ -106,12 +106,13 @@ impl DecNumRepr {
             signif10 = u256_truncating_mul_u512(&(signif2 << shl), &luv) >> shr;
             upper_signif10 =
                 u256_truncating_mul_u512(&(upper_signif2 << shl), &luv) >> shr;
+            // exp2 >= 0 => rem_zero = signif10 % 10ᵍ == 0 = signif2 % 5ᵍ == 0
+            // Analog for the lower and upper bound.
             // exp2 >= 0 => g >= 0
-            // signif10 and its lower and upper bounds have atmost 226 bits
-            // (237 + 501 - 512).
-            // g > 98 => 5ᵍ > 2²²⁶ => signif10 and its bounds can't be a
+            // signif2 and its lower and upper bounds have atmost 239 bits.
+            // g > 102 => 5ᵍ > 2²³⁹ => signif2 and its bounds can't be a
             // multiple of 5ᵍ.
-            if g <= 98 {
+            if g <= 102 {
                 // Only one of lower_signif10, signif10, upper_signif10 can be a
                 // multiple of 5, if any.
                 if signif2 % 5_u32 == 0 {
@@ -141,50 +142,99 @@ impl DecNumRepr {
             signif10 = u256_truncating_mul_u512(&(signif2 << shl), &luv) >> shr;
             upper_signif10 =
                 u256_truncating_mul_u512(&(upper_signif2 << shl), &luv) >> shr;
+            // exp2 < 0 => rem_zero = signif10 % 10ᵍ == 0 = signif2 % 2ᵍ == 0
+            // Analog for the lower and upper bound.
             if g <= 1 {
+                // signif2 = 4 * f.significand, so it has atleast 2 trailing
+                // zero bits.
                 rem_zero = true;
                 if accept_bounds {
+                    // lower_signif2 = signif2 - 1 - is_non_integer, so it has
+                    // a trailing zero bit if f is not an integer.
                     lower_rem_zero = is_non_integer;
                 } else {
+                    // uppper_signif2 = signif2 + 2, so it always has at least
+                    // one trailing zero bit.
                     signif10.decr();
                 }
+            } else if g <= 238 {
+                // signif2 has atmost 239 bits, i.e atmost 238 trailing zeroes.
+                rem_zero = signif2.trailing_zeros() >= g as u32;
             }
         }
 
         // Step 4: Find the shortest, correctly-rounded representation within
         // this interval.
+        println!("{f:?}");
+        println!("{lower_rem_zero} {rem_zero}");
         let mut i = 0_i32;
-        let (mut quot, mut round_digit) = signif10.divmod_pow10(1);
-        let (mut lower_quot, mut lower_rem) = lower_signif10.divmod_pow10(1);
-        let (mut upper_quot, mut upper_rem) = upper_signif10.divmod_pow10(1);
-        while lower_quot < upper_quot {
-            signif10 = quot;
-            lower_signif10 = lower_quot;
-            upper_signif10 = upper_quot;
-            (quot, round_digit) = signif10.divmod_pow10(1);
-            (lower_quot, lower_rem) = lower_signif10.divmod_pow10(1);
-            (upper_quot, upper_rem) = upper_signif10.divmod_pow10(1);
-            rem_zero &= round_digit == 0;
-            lower_rem_zero &= lower_rem == 0;
-            i += 1;
-        }
-        if lower_rem_zero {
-            while lower_rem == 0 && !lower_signif10.is_zero() {
+        let mut round_digit = 0_u64;
+        if lower_rem_zero || rem_zero {
+            let (mut lower_quot, mut lower_rem) =
+                lower_signif10.divmod_pow10(1);
+            let (mut upper_quot, mut upper_rem) =
+                upper_signif10.divmod_pow10(1);
+            while lower_quot < upper_quot {
+                (signif10, round_digit) = signif10.divmod_pow10(1);
+                rem_zero &= round_digit == 0;
+                lower_signif10 = lower_quot;
+                lower_rem_zero &= lower_rem == 0;
+                upper_signif10 = upper_quot;
+                i += 1;
+                (lower_quot, lower_rem) = lower_signif10.divmod_pow10(1);
+                (upper_quot, upper_rem) = upper_signif10.divmod_pow10(1);
+            }
+            if lower_rem_zero {
+                while lower_rem == 0 && !lower_signif10.is_zero() {
+                    (signif10, round_digit) = signif10.divmod_pow10(1);
+                    rem_zero &= round_digit == 0;
+                    lower_signif10 = lower_quot;
+                    (lower_quot, lower_rem) = lower_signif10.divmod_pow10(1);
+                    i += 1;
+                }
+            }
+            if round_digit > 5  // need to round up
+                || (round_digit == 5    // need to round to even
+                && (!rem_zero || (rem_zero && (signif10.lo & 1) == 1)))
+                || signif10 == lower_signif10
+            // signif10 out of bounds
+            {
+                signif10.incr();
+            }
+        } else {
+            // Can't have a tie.
+            let mut round_up = false;
+            // First, try to remove two digits.
+            let (mut lower_quot, mut lower_rem) =
+                lower_signif10.divmod_pow10(2);
+            let (mut upper_quot, mut upper_rem) =
+                upper_signif10.divmod_pow10(2);
+            if upper_quot > lower_quot {
+                let (quot, rem) = signif10.divmod_pow10(2);
+                round_up = rem >= 50;
                 signif10 = quot;
                 lower_signif10 = lower_quot;
-                (quot, round_digit) = signif10.divmod_pow10(1);
+                upper_signif10 = upper_quot;
+                i += 2;
+            }
+            (lower_quot, lower_rem) = lower_signif10.divmod_pow10(1);
+            (upper_quot, upper_rem) = upper_signif10.divmod_pow10(1);
+            while upper_quot > lower_quot {
+                (signif10, round_digit) = signif10.divmod_pow10(1);
+                round_up = round_digit >= 5;
+                lower_signif10 = lower_quot;
+                upper_signif10 = upper_quot;
                 (lower_quot, lower_rem) = lower_signif10.divmod_pow10(1);
-                rem_zero &= round_digit == 0;
+                (upper_quot, upper_rem) = upper_signif10.divmod_pow10(1);
                 i += 1;
             }
+            if round_up || signif10 == lower_signif10 {
+                signif10.incr();
+            }
         }
-        if round_digit > 5
-            || (round_digit == 5
-                && (!rem_zero || (rem_zero && (signif10.lo & 1) == 1)))
-        {
-            signif10.incr();
-        }
+        // Adjust exponent by adding number of removed digits
         exp10 += i;
+
         Self {
             sign,
             exp10,
@@ -228,6 +278,34 @@ mod tests {
     }
 
     #[test]
+    fn test_one_half() {
+        let f = f256::encode(0, -1, u256::new(0, 1));
+        let r = DecNumRepr::from_f256_shortest(&f);
+        assert_eq!(
+            r,
+            DecNumRepr {
+                sign: 0,
+                exp10: -1,
+                signif10: u256::new(0, 5)
+            }
+        )
+    }
+
+    #[test]
+    fn test_one_sixteenth() {
+        let f = f256::encode(0, -4, u256::new(0, 1));
+        let r = DecNumRepr::from_f256_shortest(&f);
+        assert_eq!(
+            r,
+            DecNumRepr {
+                sign: 0,
+                exp10: -4,
+                signif10: u256::new(0, 625)
+            }
+        )
+    }
+
+    #[test]
     fn test_f256_max() {
         let f = f256::MAX;
         let r = DecNumRepr::from_f256_shortest(&f);
@@ -238,7 +316,7 @@ mod tests {
                 exp10: 78842,
                 signif10: u256::new(
                     473526069559795162737608364600986,
-                    168794288209602616731974382256735511566
+                    168794288209602616731974382256735511567
                 )
             }
         )
